bedf333265a855c502c49da728ff49b204453bbc
WilfredLe <111155149+WilfredLe@users.noreply.github.com>
2023-07-18 22:19:52 -0700

    Update draft
    Added more details to initial concept of form. Functionality of form is finalized. Styling needs to be polished.

--- script.js
+++ script.js
@@ -2,7 +2,32 @@
 const mainPw = document.querySelector("#password");
 const confirmPw = document.querySelector("#pass-confrim");
 const inputs = document.querySelectorAll('input');
+const nameInput = document.querySelector('#first-name');
+const crewInput = document.querySelector('#crew-name')
+const roleInput = document.querySelector('select')
+const cardName = document.querySelector('.name-card');
+const crewName = document.querySelector('.crew-card');
+const roleName = document.querySelector('.role-card');
+const imgCard = document.querySelector('.profile');
 let pwContainer = 0;
+
+//Card Info Filling Functionality
+nameInput.addEventListener('change', 
+(e) => {
+        cardName.textContent = e.target.value.toUpperCase()
+    }
+);
+crewInput.addEventListener('change', 
+(e) => {
+        crewName.textContent = e.target.value.toUpperCase()
+    }
+);
+roleInput.addEventListener('change', 
+(e) => {
+        roleName.textContent = e.target.value.toUpperCase();
+        if(e.target.value === "Captain") {imgCard.setAttribute('src','HeadShot1.jpg')}
+    }
+)
 
 //Main Password Functionality
 mainPw.addEventListener('focusout',
